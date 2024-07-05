6909a1d8accfd88f8f3a0b8cffbe2d3d1023a75b
Georgia Martinez <georgia.martinez@atomicobject.com>
2024-07-05 18:08:19 -0400

    Modified drum machine

--- project1/drum_machine.py
+++ project1/drum_machine.py
@@ -4,8 +4,8 @@ import time
 from sound import Sound
 
 class DrumMachine:
-    def __init__(self):
-        self.set_bpm(100)
+    def __init__(self, bpm=100):
+        self.set_bpm(bpm)
 
         self.sound_map = {
             0: None,
@@ -21,29 +21,21 @@ class DrumMachine:
         self.pause = 0.25 * (60 / bpm) 
 
     def play(self, measure):
-        running = True
-
-        while running:
-            for event in pygame.event.get():
-                if event.type == pygame.QUIT:
-                    running = False
-
+        while True:
             for beat in range(len(measure)):
                 if measure[beat] != 0:
                     self.sound_map[measure[beat]].play()
 
                 time.sleep(self.pause)
 
-        pygame.quit()
-
 if __name__ == "__main__":
     drum_machine = DrumMachine()
 
     measure = [
-        3, 0, 1, 0,
-        2, 0, 1, 0,
-        3, 0, 1, 0,
-        2, 0, 1, 0,
+        3, 1, 1, 1,
+        2, 1, 1, 3,
+        1, 3, 1, 3,
+        2, 1, 1, 1,
     ]
 
     drum_machine.play(measure)
